113762a8f7a72e12eb2bcd7f07a0579a34b4175b
Ivan Bimbi <44509630+ibimbi@users.noreply.github.com>
2019-01-11 11:50:40 +0100

    Update registracija.php

--- Zadatak 3/registracija.php
+++ Zadatak 3/registracija.php
@@ -1,19 +1,19 @@
 <div class="reveal" id="exampleModal2" data-reveal>
         <h1 style="text-align:center;">Novi korisnik</h1>
         <p class="lead" style="text-align: center";>Vaše ime</p>
-        <input type="text" id="ime" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="text" id="ime" placeholder="Ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Vaše prezime</p>
-        <input type="text" id="prezime" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="text" id="prezime" placeholder="Prezime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Email adresa</p>
         <input type="email" id="mail" placeholder="email adresa" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Država</p>
-        <input type="text" id="drzava" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="text" id="drzava" placeholder="Država" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Grad</p>
-        <input type="text" id="grad" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="text" id="grad" placeholder="Grad" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Ulica</p>
-        <input type="text" id="ulica" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="text" id="ulica" placeholder="Ulica" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <p class="lead" style="text-align: center";>Datum rodenja</p>
-        <input type="date" placeholder="Korisničko ime" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
+        <input type="date" placeholder="datum" style="text-align: center" aria-describedby="exampleHelpTex" data-abide-ignore>
         <div class="cell small-12">
           <p class="lead" style="text-align: center"; >Lozinka</p>
           <input type="password" id="password" style="text-align: center" placeholder="Lozinka" aria-describedby="exampleHelpText" required >
